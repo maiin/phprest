f5cb5c4abd80719cbb53a3c4c253465a99c39fb1
maiin <46974772+maiin@users.noreply.github.com>
2026-03-14 15:12:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') exit;
 $configFile = __DIR__ . '/config.php';
 $pluginFile = __DIR__ . '/plugins.php';
 $secret = getenv('SECRET_CODE') ?: null;
-if (!$secret) api(['error' => 'Server misconfiguration'], 500);
+if (!$secret) api(['error' => 'Env variable not found'], 500);
 
 function createSlug($str) { return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $str), '-')); }
 function api($data, $code = 200) { http_response_code($code); echo json_encode($data, JSON_PRETTY_PRINT); exit; }
