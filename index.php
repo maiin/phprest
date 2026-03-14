c692efd206611f756d24cf62840fe8fef985be7a
maiin <46974772+maiin@users.noreply.github.com>
2026-03-14 15:12:18 +0100

    Update index.php
    Moved code to environment variable for better safety

--- index.php
+++ index.php
@@ -19,7 +19,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') exit;
 
 $configFile = __DIR__ . '/config.php';
 $pluginFile = __DIR__ . '/plugins.php';
-$secret = "PHABLE_SECRET_9921"; // IMPORTANT: Change this in production
+$secret = getenv('SECRET_CODE') ?: null;
+if (!$secret) api(['error' => 'Server misconfiguration'], 500);
 
 function createSlug($str) { return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $str), '-')); }
 function api($data, $code = 200) { http_response_code($code); echo json_encode($data, JSON_PRETTY_PRINT); exit; }
